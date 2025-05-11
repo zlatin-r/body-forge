c13342006a781a9ac2d359053a83712d02b8b597
zlatin-r <zlatinrusev@gmail.com>
2025-05-11 17:43:44 +0200

    edited

--- body_forge/settings.py
+++ body_forge/settings.py
@@ -126,3 +126,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.AppUser'
