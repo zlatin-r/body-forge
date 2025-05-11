0a27be0cf6cfe6698658c1ee78b7f5a7d0e3a15c
zlatin-r <zlatinrusev@gmail.com>
2025-05-11 18:14:49 +0200

    edited

--- body_forge/accounts/models/app_user.py
+++ body_forge/accounts/models/app_user.py
@@ -2,6 +2,8 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
+from body_forge.accounts.managers import AppUserManager
+
 
 class AppUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True,)
@@ -13,4 +15,3 @@ class AppUser(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = []
 
     objects = AppUserManager()
-    
