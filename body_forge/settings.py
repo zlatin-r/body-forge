470010ae7e8f8b29b6ae03ede7a67b142d2756be
zlatin-r <zlatinrusev@gmail.com>
2025-05-19 20:46:27 +0200

    edited

--- body_forge/settings.py
+++ body_forge/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,8 +124,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / "mediafiles"
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
