524f318ffef58f2b5473eb2e659e17b32a0a6784
zlatin-r <zlatinrusev@gmail.com>
2025-05-29 15:12:57 +0200

    changes are made

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,5 @@
-urlpatterns = [
+from django.urls import path
 
+urlpatterns = [
+    path('register/')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model, login
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from accounts.forms import AppUserCreationForm
+
+UserModel = get_user_model()
+
+
+class AppUserRegisterView(CreateView):
+    model = UserModel
+    form_class = AppUserCreationForm
+    template_name = "accounts/register-page.html"
+    success_url = reverse_lazy("home-page")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return response
