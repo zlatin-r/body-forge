adf4e9e2f713d814a814d795d1651e470b0e6076
zlatin-r <zlatinrusev@gmail.com>
2025-06-01 17:58:43 +0200

    changes are made

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path, include
 
 from accounts import views
@@ -5,7 +6,9 @@ from accounts import views
 urlpatterns = [
     path('register/', views.AppUserRegisterView.as_view(), name='register'),
     path('login/', views.AppUserLoginView.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(), name="logout"),
     path('<int:pk>/', include([
+        path('details/', views.ProfileDetailsView.as_view(), name='profile-details'),
         path('edit/', views.ProfileEditView.as_view(), name='profile-edit'),
     ])),
 ]
