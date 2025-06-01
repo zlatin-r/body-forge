5d99cd0ecc7a015df8c3f2de4279d3bcd24dbbd2
zlatin-r <zlatinrusev@gmail.com>
2025-06-01 17:15:28 +0200

    changes are made

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
+from accounts.models import Profile
+
 UserModel = get_user_model()
 
 
@@ -15,3 +17,19 @@ class AppUserCreationForm(UserCreationForm):
             'password1': forms.PasswordInput(attrs={"class": "form-control"}),
             'password2': forms.PasswordInput(attrs={"class": "form-control"}),
         }
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ("user", "date_joined", "date_of_birth")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['profile_picture'].widget = forms.FileInput(attrs={
+            'accept': 'image/*'
+        })
+
+        self.fields['height'].help_text = "in centimeters"
+        self.fields['body_weight'].help_text = "in kilograms"
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 
 from accounts import views
 
 urlpatterns = [
     path('register/', views.AppUserRegisterView.as_view(), name='register'),
     path('login/', views.AppUserLoginView.as_view(), name="login"),
+    path('<int:pk>/', include([
+        path('edit/', views.ProfileEditView.as_view(), name='profile-edit'),
+    ])),
 ]
