44d62c41a626b3bfe575fa781bdc93abd050bc3b
zlatin-r <zlatinrusev@gmail.com>
2025-06-15 17:25:41 +0200

    changes are made

--- workouts/urls.py
+++ workouts/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-
-from workouts import views
+from workouts.views import *
 
 urlpatterns = [
-    path('start/', views.StartWorkoutView.as_view(), name='start-workout'),
-    path('add-sets/<int:workout_id>/', views.AddWorkoutSetsView.as_view(), name='add-workout-sets'),
-    path('summary/<int:workout_id>/', views.WorkoutSummaryView.as_view(), name='workout-summary'),
+    path('start/', StartWorkoutView.as_view(), name='start-workout'),
+    path('add-sets/<int:workout_id>/', AddWorkoutSetsView.as_view(), name='add-workout-sets'),
+    path('summary/<int:workout_id>/', WorkoutSummaryView.as_view(), name='workout-summary'),
 ]
