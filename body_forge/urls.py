fe853412752041f10902074704b23f3ab11de3e1
zlatin-r <zlatinrusev@gmail.com>
2025-06-15 16:56:41 +0200

    changes are made

--- body_forge/urls.py
+++ body_forge/urls.py
@@ -1,15 +1,15 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.static import serve
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('common.urls')),
     path('accounts/', include('accounts.urls')),
+    path('workout/', include('workouts.urls')),
 
 ]
 
-urlpatterns += [
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
