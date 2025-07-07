ca79105eef373972f7716514a2a11b836bd6a87c
zlatin-r <zlatinrusev@gmail.com>
2025-07-07 11:36:30 +0300

    changes are made

--- accounts/models/app_user.py
+++ accounts/models/app_user.py
@@ -6,10 +6,16 @@ from accounts.managers import AppUserManager
 
 
 class AppUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=True)
+    email = models.EmailField(
+        unique=True
+    )
 
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(
+        default=True
+    )
+    is_staff = models.BooleanField(
+        default=False
+    )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
