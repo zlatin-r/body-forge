d02ef8e4533c636d1fdd0c1ca3d01fe9a5dcf545
zlatin-r <zlatinrusev@gmail.com>
2025-07-09 11:35:44 +0300

    workout app init

--- body_forge/urls.py
+++ body_forge/urls.py
@@ -7,8 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('common.urls')),
     path('accounts/', include('accounts.urls')),
-    # path('workout/', include('workouts.urls')),
-
+    path('workout/', include('workouts.urls')),
 ]
 
 if settings.DEBUG:
