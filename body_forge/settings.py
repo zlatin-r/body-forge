47d357d067735194f20056dc495113a6de4e445d
zlatin-r <zlatinrusev@gmail.com>
2025-08-13 12:48:05 +0300

    fix: commited background images

--- body_forge/settings.py
+++ body_forge/settings.py
@@ -68,6 +68,12 @@ TEMPLATES = [
 ]
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+        "OPTIONS": {
+            "location": BASE_DIR / "media",
+        },
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
--- static_files/javascript/index.js
+++
@@ -1,6 +0,0 @@
-import './navbar/dropdownMenu.js';
-import './profilePicChange/profilePicChange.js';
-import './workout/startExercise.js';
-import './workout/deleteWtType.js';
-import './workout/deleteExercise.js';
-import './workout/deleteMGroup.js';
